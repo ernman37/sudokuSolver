51eaec4d5534618af2fcc794f829c47c16e1f0ea
ernman37 <Rion.duckworth@gmail.com>
2021-07-01 23:27:45 -0600

    Cleaned

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -10,27 +10,40 @@
 #include <string>
 #include <chrono>
 
+//Includes namespaces and defines new type of float seconds
 using namespace std;
 using namespace std::chrono;
 using floatsec = duration<double, ratio<1,1>>;
 
+
+//Constructor which auto solves the puzzle
 Sudoku::Sudoku(){ 
     getInput(); 
-    cout << "Puzzel Entered: \n" << *this;
+
     auto start = steady_clock::now();
-    bool ran = solve(0,0);
+    bool ran = solve();
     auto ms = duration_cast<milliseconds>(steady_clock::now() - start);
     floatsec s = ms;
+
     if(ran) cout << "Solved: \n" << *this;
     else cout << "Unsolvable Puzzel\n";
+
     cout << counter << " iterations in " << s.count() << " seconds or " << ms.count() << " milliseconds\n";
 }
 
+//Gets input from user
+//Format in solid numbers no spaces 
+//0's represent unmarked spot
 void Sudoku::getInput(){
+    //loop for all 9 rows
     for (int i = 0; i < 9; i++){
         cout << "Input Row #" << i+1 << '\n';
         string line = "";
-        while (line.length() != 9) getline(cin, line);
+
+        while (line.length() != 9)
+            getline(cin, line);
+
+        //loop for all 9 colums
         for (int j = 0; j < 9; j++){
             int num = ((int)line[j]) - 48;
             if(num > 9 || num < 0){
@@ -40,31 +53,27 @@ void Sudoku::getInput(){
             table[i][j] = num;
         }
     }
-}
-
-void Sudoku::print(){
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++)
-           cout << table[i][j] << "  ";
-        cout << '\n';
-    }
+    cout << "Puzzel Entered: \n" << *this;
 }
 
 
-
+//Checks every Row for the c colum for the n number
 bool Sudoku::checkR(const int &c, const int &n){
     for(int i = 0; i < 9; i++)
        if(table[i][c] == n) return false;
     return true;
-
 }
 
+
+//Checks every colum in the r row for the n number
 bool Sudoku::checkC(const int &r, const int &n){
     for(int i = 0; i < 9; i++)
        if(table[r][i] == n) return false;
     return true;
 }
 
+
+//Checks corresponding box for r & c position for n number
 bool Sudoku::checkB(const int &r, const int &c, const int &n){
     int br = r - (r % 3),
         bc = c - (c % 3);
@@ -75,39 +84,47 @@ bool Sudoku::checkB(const int &r, const int &c, const int &n){
     return true;
 }
 
+
+//Checks if n fits requirements for certain spot
 bool Sudoku::tryN(const int &r, const int &c, const int &n){
     return (checkR(c,n) && checkC(r,n) && checkB(r,c,n));
 }
 
+
+//Solves the table
 bool Sudoku::solve(int row, int col){
     //End of the table has been reached
     if(row == 8 && col == 9) return true;
+
     //Iterate to next row
     if(col == 9){
        row++;
        col = 0;
     }
+
     //Skips already solved slots
-    if(table[row][col] != 0) return solve(row, col+1);
-    //Trys every number in a loop and removes bad ones
+    if(table[row][col] != 0) return solve(row, col+1); 
+
+    //Trys every number in a recursive loop
     for(int num = 1; num < 10; num++){
        if(tryN(row, col, num)){
-          counter++;
+          counter++; 
           table[row][col] = num;
-          if(solve(row, col+1))
-             return true;
+          if(solve(row, col+1)) return true;
        }
-       //resets the row
-       table[row][col] = 0;
+       table[row][col] = 0; //Resets if passed number fails
     }
     return false;
 }
 
 
+//Returns iteration counter
 long long Sudoku::getCounter(){
    return counter;
 }
 
+
+//Outputs the table
 ostream& operator<<(ostream &os, Sudoku &copy){
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++)
--- Sudoku.hpp
+++ Sudoku.hpp
@@ -1,34 +1,34 @@
 /*
 *    Creator: Ernest M Duckworth IV
 *    Date: Thu Jul  1 14:23:34 2021
-*    For: Class Declaration for object: Sudoku
-*    Description: 
+*    Class Declaration for object: Sudoku
+*    Description: The body of an object that solves sudoku puzzles 
 */
 #ifndef Sudoku_hpp
 #define Sudoku_hpp
 
 #include <iostream>
-#include <string>
 
 class Sudoku{
   private:
+    //Variables
     int table[9][9];
     long long counter = 0;
 
   public:
+    //Helper and initialization functions
     Sudoku();
     void getInput();
     auto getTable() { return table; }
-    void print();
-
+    long long getCounter();
+    friend std::ostream& operator<<(std::ostream &, Sudoku &);
+    
+    //Testing functions
     bool checkR(const int &, const int &);
     bool checkC(const int &, const int &);
     bool checkB(const int &, const int &, const int &);
     bool tryN(const int &, const int &, const int &);
-    bool solve(int row, int col);
-
-    long long getCounter();
-    friend std::ostream& operator<<(std::ostream &, Sudoku &);
+    bool solve(int row=0, int col=0);
 };
 
 #endif
